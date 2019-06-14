9c4a7f11d05d5eab2f9f057653a68a20a4cc7a1e
Edson Armando <36713890+SistemaRayoXP@users.noreply.github.com>
2019-06-14 12:42:20 -0500

    Updated project to VS2010

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' mediante el asterisco ('*'), como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.2.0.*")> 
+<Assembly: AssemblyVersion("1.2.0.0")> 
 <Assembly: AssemblyFileVersion("1.0.2.0")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
